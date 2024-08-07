efbf4d5a4786161c3101a6d28dc357f1c57184f8
Asim313 <asimkhurshid313@gmail.com>
2024-08-07 13:24:33 +0500

    updated

--- App.tsx
+++ App.tsx
@@ -1,22 +1,70 @@
-import React from 'react';
-import Navigation from './src/navigation/Navigation';
-import {StatusBar, View} from 'react-native';
-import {
-  GestureHandlerRootView,
-} from 'react-native-gesture-handler';
-import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { useRingerMode, RINGER_MODE } from 'react-native-ringer-mode';
+import BackgroundFetch from 'react-native-background-fetch';
 
-// Main App component
-export default function App() {
+const App = () => {
+  const { setMode } = useRingerMode();
+
+  useEffect(() => {
+    const configureBackgroundFetch = () => {
+      BackgroundFetch.configure(
+        {
+          minimumFetchInterval: 1, // minutes
+          stopOnTerminate: false,
+          startOnBoot: true,
+          enableHeadless: true,
+          requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE, // Default
+          requiresCharging: false, // Default
+          requiresDeviceIdle: false, // Default
+          requiresBatteryNotLow: false, // Default
+          requiresStorageNotLow: false, // Default
+        },
+        async (taskId) => {
+          const now = new Date();
+          const currentHour = now.getHours();
+          const currentMinute = now.getMinutes();
+
+          console.log(currentHour, currentMinute)
+          
+          if (currentHour == 1 && currentMinute === 7) {
+            // Set to silent mode at 10 PM
+            await setMode(RINGER_MODE.silent);
+          } else if (currentHour == 1 && currentMinute === 8) {
+            // Set to normal mode at 6 AM
+            await setMode(RINGER_MODE.normal);
+          }
+
+          BackgroundFetch.finish(taskId);
+        },
+        (error) => {
+          console.log('BackgroundFetch failed to start', error);
+        }
+      );
+
+      // Optional: Schedule a background-fetch event every 15 minutes
+      BackgroundFetch.scheduleTask({
+        taskId: 'com.myapp.silent_unsilent_mode',
+        delay: 15 * 60 * 1000, // milliseconds
+        periodic: true,
+        forceAlarmManager: true,
+        stopOnTerminate: false,
+        enableHeadless: true,
+      });
+    };
+
+    configureBackgroundFetch();
+
+    return () => {
+      BackgroundFetch.stop();
+    };
+  }, [setMode]);
+
   return (
-    // Provide Redux store to the app
-    <View style={{flex: 1, backgroundColor: '#000'}}>
-      <StatusBar translucent backgroundColor={'transparent'} />
-      <GestureHandlerRootView style={{flex: 1}}>
-        <BottomSheetModalProvider>
-          <Navigation />
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
+    <View>
+      <Text>Scheduling Silent and Unsilent Modes</Text>
     </View>
   );
-}
+};
+
+export default App;
